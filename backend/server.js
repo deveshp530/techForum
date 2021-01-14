ab6ff59d6d249ab4999bb605d84b89ad58189caf
Devesh Patel <deveshp530@gmail.com>
2021-01-14 13:00:22 -0800

    setting up user auth

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,34 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cors({origin: process.env.CLIENT_URL}))
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('db connected'))
+  .catch((err) => console.error(err));
 
-const authRoute = require('./routes/auth')
+mongoose.connection.on('error', (err) => {
+  console.log(`DB connections error: ${err.me}`);
+});
 
-const port = process.env.PORT || 8000
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors({ origin: process.env.CLIENT_URL }));
 
-app.use('/api', authRoute)
+const authRoute = require('./routes/auth');
 
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`);
-})
+const port = process.env.PORT || 8000;
+
+app.use('/api', authRoute);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
